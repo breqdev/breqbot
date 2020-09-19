68853a104471df78075143dfc8dc3888c4d9cf34
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-09-19 14:09:32 -0400

    Combine debug and info cogs

--- breqbot.py
+++ breqbot.py
@@ -14,7 +14,7 @@ breqbot.redis = redis.Redis.from_url(os.getenv("REDIS_URL"), decode_responses=Tr
 
 breqbot.load_extension("reddit")
 breqbot.load_extension("currency")
-breqbot.load_extension("debug")
+breqbot.load_extension("info")
 breqbot.load_extension("inventory")
 breqbot.load_extension("quests")
 breqbot.load_extension("wear")
@@ -58,19 +58,5 @@ async def on_guild_leave(guild):
     breqbot.redis.srem("guild:list", guild.id)
     breqbot.redis.delete(f"guild:name:{guild.id}")
     breqbot.redis.delete(f"guild:member:{guild.id}")
-
-@breqbot.command()
-async def website(ctx, user: typing.Optional[discord.User]):
-    "Link to the bot's website!"
-    if user:
-        await ctx.send(f"{os.getenv('WEBSITE')}user/{ctx.guild.id}/{user.id}")
-    else:
-        await ctx.send(f"{os.getenv('WEBSITE')}server/{ctx.guild.id}")
-
-@breqbot.command()
-async def testing(ctx):
-    "Come join the bot testing server to suggest features and discuss Breqbot!"
-    await ctx.send("Join us and discuss features for Breqbot!")
-    await ctx.send(os.getenv("TESTING_DISCORD"))
 
 breqbot.run(os.getenv("DISCORD_TOKEN"))
--- info.py
+++ info.py
@@ -1,14 +1,29 @@
 import os
 import time
+import typing
 
 import discord
 from discord.ext import commands
 
 startup_timestamp = time.time()
 
-class Debug(commands.Cog):
+class Info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def website(self, ctx, user: typing.Optional[discord.User]):
+        "Link to the bot's website!"
+        if user:
+            await ctx.send(f"{os.getenv('WEBSITE')}user/{ctx.guild.id}/{user.id}")
+        else:
+            await ctx.send(f"{os.getenv('WEBSITE')}server/{ctx.guild.id}")
+
+    @commands.command()
+    async def testing(self, ctx):
+        "Come join the bot testing server to suggest features and discuss Breqbot!"
+        await ctx.send("Join us and discuss features for Breqbot!")
+        await ctx.send(os.getenv("TESTING_DISCORD"))
 
     @commands.command()
     async def ping(self, ctx):
@@ -38,4 +53,4 @@ class Debug(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(Debug(bot))
+    bot.add_cog(Info(bot))
