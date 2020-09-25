6dc8e8589a71e9c85a0ca3cd10077a0213750c23
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-09-24 21:04:43 -0400

    Rename "cogs" dir to "extensions"

--- breqbot.py
+++ breqbot.py
@@ -13,27 +13,27 @@ breqbot = commands.Bot(prefix, description="Hi, I'm Breqbot! Beep boop :robot:",
 breqbot.redis = redis.Redis.from_url(os.getenv("REDIS_URL"), decode_responses=True)
 
 # General
-breqbot.load_extension("cogs.info")
+breqbot.load_extension("extensions.info")
 
 # External Features
-breqbot.load_extension("cogs.reddit")
-breqbot.load_extension("cogs.minecraft")
-breqbot.load_extension("cogs.vex")
-breqbot.load_extension("cogs.comics")
+breqbot.load_extension("extensions.reddit")
+breqbot.load_extension("extensions.minecraft")
+breqbot.load_extension("extensions.vex")
+breqbot.load_extension("extensions.comics")
 
 # Breqbot Features
-breqbot.load_extension("cogs.soundboard")
-breqbot.load_extension("cogs.portal")
+breqbot.load_extension("extensions.soundboard")
+breqbot.load_extension("extensions.portal")
 
 # Economy/shop/outfits
-breqbot.load_extension("cogs.website")
-breqbot.load_extension("cogs.currency")
-breqbot.load_extension("cogs.inventory")
-breqbot.load_extension("cogs.quests")
-breqbot.load_extension("cogs.wear")
+breqbot.load_extension("extensions.website")
+breqbot.load_extension("extensions.currency")
+breqbot.load_extension("extensions.inventory")
+breqbot.load_extension("extensions.quests")
+breqbot.load_extension("extensions.wear")
 
 # Utility
-breqbot.load_extension("cogs.help_command")
-breqbot.load_extension("cogs.guild_watch")
+breqbot.load_extension("extensions.help_command")
+breqbot.load_extension("extensions.guild_watch")
 
 breqbot.run(os.getenv("DISCORD_TOKEN"))
