98242192f271bbb641ae4695a8becf26771559bd
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-09-30 01:48:05 -0400

    Use Redis to ensure reddit submissions aren't repeated

--- extensions/reddit.py
+++ extensions/reddit.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import random
 
 import praw
@@ -16,7 +17,7 @@ reddit = praw.Reddit(client_id=os.getenv("REDDIT_CLIENT_ID"),
 
 
 @run_in_executor
-def get_posts(sub_name, nsfw=None, spoiler=None, flair=None):
+def get_posts(sub_name, channel=None, redis=None, nsfw=None, spoiler=None, flair=None):
     sub = reddit.subreddit(sub_name)
 
     try:
@@ -27,19 +28,27 @@ def get_posts(sub_name, nsfw=None, spoiler=None, flair=None):
     if nsfw is False and sub.over18:
         raise Fail("NSFW content is limited to NSFW channels only.")
 
+    # Clear old posts
+    now = time.time()
+    # long_ago = now - 3600  # 1 hr ago
+
+    if channel:
+        # redis.zremrangebyscore(f"reddit:{channel}", 0, long_ago)
+        redis.zremrangebyscore(f"reddit:{channel}", 0, -50)
+
     frontpage = sub.top("month", limit=1000)
-    images = []
     for submission in frontpage:
-        if len(images) > 25:
-            break
         if submission.is_self:
             continue
+
         if spoiler is not None:
             if submission.spoiler != spoiler:
                 continue
+
         if nsfw is not None:
             if submission.over_18 != nsfw:
                 continue
+
         if flair is not None:
             if submission.link_flair_text is None:
                 continue
@@ -48,15 +57,16 @@ def get_posts(sub_name, nsfw=None, spoiler=None, flair=None):
                     break
             else:
                 continue
-        images.append(submission.url)
-    else:
-        pass
-        # print("Ran out of posts!")
-    if images:
-        image = random.choice(images)
-        return image
-    else:
-        return "No images found!"
+
+        if channel:
+            if redis.zscore(f"reddit:{channel}", submission.id):
+                continue  # Submission posted recently
+
+        redis.zadd(f"reddit:{channel}", {submission.id: now})
+
+        return submission.url
+
+    return "No images found!"
 
 
 class BaseReddit(BaseCog):
@@ -67,14 +77,23 @@ class BaseReddit(BaseCog):
         also try `doki fun` or ||`doki nsfw` :smirk:||"""
         async with ctx.channel.typing():
             image = await get_posts(
-                "DDLC", spoiler=None, nsfw=("nsfw" in ctx.message.content),
-                flair=(["Fun"] if "fun" in ctx.message.content
-                       else ["Fanart", "Media"]))
+                "DDLC",
+                channel=ctx.channel.id,
+                redis=self.redis,
+                spoiler=None,
+                nsfw=("nsfw" in ctx.message.content),
+                flair=(["Fun"] if "fun" in ctx.message.content else ["Fanart", "Media"])
+            )
         return image
 
     async def default(self, ctx, subreddit):
         async with ctx.channel.typing():
-            image = await get_posts(subreddit, nsfw=False)
+            image = await get_posts(
+                subreddit,
+                channel=ctx.channel.id,
+                redis=self.redis,
+                nsfw=(None if ctx.channel.is_nsfw() else False)
+            )
         return image
 
     @commands.command()
@@ -84,7 +103,11 @@ class BaseReddit(BaseCog):
         channel_is_nsfw = ctx.guild and ctx.channel.is_nsfw()
         async with ctx.channel.typing():
             image = await get_posts(
-                subreddit, nsfw=(None if channel_is_nsfw else False))
+                subreddit,
+                channel=ctx.channel.id,
+                redis=self.redis,
+                nsfw=(None if channel_is_nsfw else False)
+            )
         return image
 
 
