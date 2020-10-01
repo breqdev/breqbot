b90531adff0dd5a6a23a43a08d2fbb391cf2331f
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-10-01 17:36:40 -0400

    Add a basic singleplayer roulette

--- extensions/currency.py
+++ extensions/currency.py
@@ -1,4 +1,5 @@
 import typing
+import random
 
 import discord
 from discord.ext import commands
@@ -120,6 +121,76 @@ class Currency(BaseCog):
         item = Item.from_name(self.redis, ctx.guild.id, item)
         self.redis.srem(f"shop:items:{ctx.guild.id}", item.uuid)
         self.redis.delete(f"shop:prices:{ctx.guild.id}:{item.uuid}")
+
+    @commands.command()
+    @commands.guild_only()
+    @passfail
+    async def roulette(self, ctx, bet: str, wager: str):
+        "Gamble coins - will you earn more, or lose them all?"
+
+        balance = int(self.redis.get(f"currency:balance:{ctx.guild.id}:{ctx.author.id}"))
+
+        if wager == "all":
+            wager = balance
+        elif wager == "none":
+            wager = 0
+        elif wager == "half":
+            wager = balance // 2
+        else:
+            try:
+                wager = int(wager)
+            except ValueError:
+                raise Fail(f"Invalid wager: {wager}")
+
+        if wager > balance:
+            raise Fail("You cannot wager more than you have.")
+        if wager < 0:
+            raise Fail("Wager must be a positive number.")
+
+        if bet not in ("red", "black", "even", "odd", "high", "low"):
+            try:
+                bet = int(bet)
+            except ValueError:
+                raise Fail(f"Invalid bet: {bet}")
+
+        self.redis.decr(f"currency:balance:{ctx.guild.id}:{ctx.author.id}", wager)
+
+        wheel = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26]
+
+        red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+        black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+
+        ball = random.choice(wheel)
+
+        color = "red" if ball in red else "black" if ball in black else "green"
+        parity = "none" if ball == 0 else "even" if ball % 2 == 0 else "odd"
+        range = "high" if ball >= 19 else "low" if ball >= 1 else "none"
+
+        color_emoji = {
+            "red": "🟥",
+            "black": "⬛",
+            "green": "🟩"
+        }
+
+        message = await ctx.send(f"{ball}: {color_emoji[color]}")
+
+        if bet == color:
+            winnings = wager * 2
+        elif bet == parity:
+            winnings = wager * 2
+        elif bet == range:
+            winnings = wager * 2
+        elif bet == ball:
+            winnings = wager * 36
+        else:
+            winnings = 0
+
+        self.redis.incr(f"currency:balance:{ctx.guild.id}:{ctx.author.id}", winnings)
+
+        if winnings > 0:
+            return f"You win **{winnings - wager}** coins!"
+        else:
+            return f"You lost **{wager}** coins."
 
 
 def setup(bot):
