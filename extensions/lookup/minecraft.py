5e7e025afd567c7e40f06fb7a2041917a1c93cd7
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-10-02 00:56:41 -0400

    The beginnings of a publish framework...

--- extensions/lookup/minecraft.py
+++ extensions/lookup/minecraft.py
@@ -1,25 +1,24 @@
+import hashlib
+
 import discord
 from discord.ext import commands
 
 from mcstatus import MinecraftServer
 
-from ..base import BaseCog, UserError
+from ..base import UserError, run_in_executor
+from ..publisher import PublisherCog
 
 
-class Minecraft(BaseCog):
+class Minecraft(PublisherCog):
     "Tools for Minecraft servers"
 
-    @commands.command()
-    async def mc(self, ctx, ip: str):
-        """:mag: :desktop: Look up information about a Minecraft server
-        :video_game:"""
+    @run_in_executor
+    def _get_state(self, ip):
         try:
             server = MinecraftServer.lookup(ip)
             status = server.status().raw
         except OSError:
             raise UserError("Could not connect to Minecraft server")
-
-        embed = discord.Embed(title=ip)
 
         description = []
 
@@ -37,19 +36,58 @@ class Minecraft(BaseCog):
         else:
             description.append(status["description"])
 
-        description = "**Description**\n" + "".join(description)
+        description = "".join(description)
 
-        playerstr = (f"Players: **{status['players']['online']}**"
-                     f"/{status['players']['max']}")
+        players = (status["players"]["online"], status["players"]["max"])
 
         if status["players"].get("sample"):
-            online = "\n" + "\n".join(f"• {player['name']}" for player
-                                      in status["players"]["sample"])
+            sample = [player["name"] for player in status["players"]["sample"]]
+        else:
+            sample = []
+
+        return description, players, sample
+
+    @commands.command()
+    async def mc(self, ctx, ip: str):
+        """:mag: :desktop: Look up information about a Minecraft server
+        :video_game:"""
+        embed = await self.get_update(ip)
+        await ctx.send(embed=embed)
+
+    async def get_hash(self, ip):
+        description, players, sample = await self._get_state(ip)
+        hashstr = f"{description} {players} {sample}"
+        hash = hashlib.sha1(hashstr.encode("utf-8")).hexdigest()
+        return hash
+
+    async def get_update(self, ip):
+        description, players, sample = await self._get_state(ip)
+        embed = discord.Embed(title=ip)
+        description = "**Description**\n" + description
+        playerstr = f"Players: **{players[0]}**/{players[1]}"
+        if sample:
+            online = "\n" + "\n".join(f"• {player}" for player in sample)
         else:
             online = ""
-
         embed.description = description + "\n" + playerstr + online
+        return embed
+
+    @commands.command()
+    async def watchmc(self, ctx, ip: str):
+        await self.register_channel(ip, ctx.channel.id)
+
+    @commands.command()
+    async def watchingmc(self, ctx):
+        watching = await self.get_watching(ctx.channel.id)
+
+        embed = discord.Embed(title="Watching IPs")
+        embed.description = "\n".join(watching)
+
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def unwatchmc(self, ctx, ip: str):
+        await self.unregister_channel(ip, ctx.channel.id)
 
 
 def setup(bot):
