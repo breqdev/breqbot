59f7b76ee86e91f3eb0c3b1ea1e939f5b9aeb549
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-10-20 18:17:30 -0400

    Use the aiohttp module for the portal client

--- portal.py
+++ portal.py
@@ -1,54 +1,44 @@
 import os
-import json
-import time
+import asyncio
 
-import websocket
+import aiohttp
 
 
 class Portal:
     def __init__(self, url, id, token):
+        self.url = url
         self.id = id
         self.token = token
 
-        self.socket = websocket.WebSocketApp(f"{url}portal")
+    async def connect(self):
+        self.session = aiohttp.ClientSession()
+        self.socket = await self.session.ws_connect(f"{self.url}portal")
 
-        def on_open(ws):
-            self.on_open()
+        await self.auth()
+        await self.set_status(2)
 
-        def on_message(ws, message):
-            self.handle_request(message)
+    async def handle(self):
+        await self.connect()
+        while True:
+            message = await self.socket.receive_json()
+            await self.handle_request(message)
 
-        def on_error(ws, error="Closed"):
-            print(f"Error: {error}")
-
-        self.socket.on_open = on_open
-        self.socket.on_message = on_message
-        self.socket.on_error = self.socket.on_close = on_error
-
-        self.request_callback = lambda self, message: None
-
-    def on_open(self):
-        self.auth()
-        self.set_status(2)
-
-    def auth(self):
-        message = json.dumps({
+    async def auth(self):
+        message = {
             "type": "auth",
             "id": self.id,
             "token": self.token
-        })
-        self.socket.send(message)
+        }
+        await self.socket.send_json(message)
 
-    def set_status(self, status):
-        message = json.dumps({
+    async def set_status(self, status):
+        message = {
             "type": "status",
             "status": status
-        })
-        self.socket.send(message)
+        }
+        await self.socket.send_json(message)
 
-    def handle_request(self, message):
-        message = json.loads(message)
-
+    async def handle_request(self, message):
         if message["type"] == "ping":
             return
         elif message["type"] != "query":
@@ -56,12 +46,12 @@ class Portal:
 
         result = self.request_callback(message["data"])
 
-        response = json.dumps({
+        response = {
             "type": "response",
             "job": message["job"],
             "data": result
-        })
-        self.socket.send(response)
+        }
+        await self.socket.send_json(response)
 
     def on_request(self):
         def decorator(func):
@@ -70,10 +60,15 @@ class Portal:
         return decorator
 
     def run(self):
-        self.socket.run_forever()
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.handle())
 
 
-portal = Portal(os.getenv("PORTAL_URL"), os.getenv("PORTAL_ID"), os.getenv("PORTAL_TOKEN"))
+portal = Portal(
+    os.getenv("PORTAL_URL"),
+    os.getenv("PORTAL_ID"),
+    os.getenv("PORTAL_TOKEN")
+)
 
 
 @portal.on_request()
