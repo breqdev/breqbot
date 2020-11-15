2a6ddf7f430c11aadfecd2387ab5c139a2791488
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-11-15 16:03:52 -0500

    Move GuildWatch into a Cog

--- extensions/utility/guild_watch.py
+++ extensions/utility/guild_watch.py
@@ -1,80 +1,90 @@
-def setup(bot):
-    @bot.event
-    async def on_ready():
-        await bot.redis.delete("guild:list")
-        await bot.redis.delete("user:list")
+from discord.ext import commands
 
-        await bot.redis.sadd("guild:list", *(guild.id for guild in bot.guilds))
+from .. import base
+
+
+class GuildWatch(base.BaseCog):
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.redis.delete("guild:list")
+        await self.redis.delete("user:list")
+
+        await self.redis.sadd(
+            "guild:list", *(guild.id for guild in self.bot.guilds))
 
         # Cache of guild ID -> name, guild member list, user ID -> name, etc
-        for guild in bot.guilds:
-            await bot.redis.hset(f"guild:{guild.id}", "name", guild.name)
+        for guild in self.bot.guilds:
+            await self.redis.hset(f"guild:{guild.id}", "name", guild.name)
 
-            await bot.redis.delete(f"guild:member:{guild.id}")
-            await bot.redis.sadd(
+            await self.redis.delete(f"guild:member:{guild.id}")
+            await self.redis.sadd(
                 f"guild:member:{guild.id}",
                 *(member.id for member in guild.members))
 
-            await bot.redis.sadd(
+            await self.redis.sadd(
                 "user:list", *(member.id for member in guild.members))
 
-        for member in bot.get_all_members():
-            await bot.redis.set(
+        for member in self.bot.get_all_members():
+            await self.redis.set(
                 f"user:name:{member.guild.id}:{member.id}",
                 member.display_name)
-            await bot.redis.hset(
+            await self.redis.hset(
                 f"profile:{member.guild.id}:{member.id}",
                 "pfp", str(member.avatar_url))
 
-    @bot.event
-    async def on_member_join(member):
-        await bot.redis.sadd(f"guild:member:{member.guild.id}", member.id)
-        await bot.redis.sadd("user:list", member.id)
-        await bot.redis.set(
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await self.redis.sadd(f"guild:member:{member.guild.id}", member.id)
+        await self.redis.sadd("user:list", member.id)
+        await self.redis.set(
             f"user:name:{member.guild.id}:{member.id}", member.display_name)
-        await bot.redis.hset(
+        await self.redis.hset(
             f"profile:{member.guild.id}:{member.id}",
             "pfp", str(member.avatar_url))
 
-    @bot.event
-    async def on_member_leave(member):
-        await bot.redis.srem(f"guild:member:{member.guild.id}", member.id)
-        await bot.redis.delete(f"user:name:{member.guild.id}:{member.id}")
-        await bot.redis.delete(f"profile:{member.guild.id}:{member.id}")
+    @commands.Cog.listener()
+    async def on_member_leave(self, member):
+        await self.redis.srem(f"guild:member:{member.guild.id}", member.id)
+        await self.redis.delete(f"user:name:{member.guild.id}:{member.id}")
+        await self.redis.delete(f"profile:{member.guild.id}:{member.id}")
 
-    @bot.event
-    async def on_member_update(old, member):
-        await bot.redis.set(
+    @commands.Cog.listener()
+    async def on_member_update(self, old, member):
+        await self.redis.set(
             f"user:name:{member.guild.id}:{member.id}", member.display_name)
-        await bot.redis.hset(
+        await self.redis.hset(
             f"profile:{member.guild.id}:{member.id}",
             "pfp", str(member.avatar_url))
 
-    @bot.event
-    async def on_user_update(old, user):
+    @commands.Cog.listener()
+    async def on_user_update(self, old, user):
         # TODO: try and detect username changes?
         pass
 
-    @bot.event
-    async def on_guild_join(guild):
-        await bot.redis.sadd("guild:list", guild.id)
-        await bot.redis.hset(f"guild:{guild.id}", "name", guild.name)
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        await self.redis.sadd("guild:list", guild.id)
+        await self.redis.hset(f"guild:{guild.id}", "name", guild.name)
 
-        await bot.redis.delete(f"guild:member:{guild.id}")
-        await bot.redis.sadd(f"guild:member:{guild.id}",
-                             *(member.id for member in guild.members))
+        await self.redis.delete(f"guild:member:{guild.id}")
+        await self.redis.sadd(f"guild:member:{guild.id}",
+                              *(member.id for member in guild.members))
 
         for member in guild.members:
-            await bot.redis.set(
+            await self.redis.set(
                 f"user:name:{member.guild.id}:{member.id}",
                 member.display_name)
-            await bot.redis.hset(
+            await self.redis.hset(
                 f"profile:{member.guild.id}:{member.id}",
                 "pfp", str(member.avatar_url))
-            await bot.redis.sadd("user:list", member.id)
+            await self.redis.sadd("user:list", member.id)
 
-    @bot.event
-    async def on_guild_leave(guild):
-        await bot.redis.srem("guild:list", guild.id)
-        await bot.redis.delete(f"guild:{guild.id}")
-        await bot.redis.delete(f"guild:member:{guild.id}")
+    @commands.Cog.listener()
+    async def on_guild_leave(self, guild):
+        await self.redis.srem("guild:list", guild.id)
+        await self.redis.delete(f"guild:{guild.id}")
+        await self.redis.delete(f"guild:member:{guild.id}")
+
+
+def setup(bot):
+    bot.add_cog(GuildWatch(bot))
