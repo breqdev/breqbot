005087c25ae23acc5a68d629f1bc4e44e4c680b6
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-11-25 02:25:44 -0500

    Watch YouTube channels for updates

--- extensions/feeds/youtube.py
+++ extensions/feeds/youtube.py
@@ -5,9 +5,10 @@ import discord
 from discord.ext import commands
 
 from .. import base
+from .. import watch
 
 
-class Youtube(base.BaseCog):
+class Youtube(base.BaseCog, watch.Watchable):
     "See info about YouTube channels"
 
     category = "Feeds"
@@ -16,6 +17,8 @@ class Youtube(base.BaseCog):
         super().__init__(bot)
         self.session = aiohttp.ClientSession()
         self.key = os.getenv("YOUTUBE_API_KEY")
+
+        self.watch = watch.ChannelWatch(self, crontab="*/15 * * * *")
 
     async def get_channel(self, search, nsfw=None):
         async with self.session.get(
@@ -35,12 +38,12 @@ class Youtube(base.BaseCog):
 
         return response["items"][0]
 
-    async def get_latest(self, channel, nsfw=None):
+    async def get_state(self, channel_id, nsfw=None):
         async with self.session.get(
                 "https://youtube.googleapis.com/youtube/v3/search",
                 params={
                     "part": "snippet",
-                    "channelId": channel["id"]["channelId"],
+                    "channelId": channel_id,
                     "maxResults": "1",
                     "order": "date",
                     "safeSearch": ("none" if nsfw else "moderate"),
@@ -53,6 +56,38 @@ class Youtube(base.BaseCog):
             raise commands.CommandError("No videos found!")
 
         return response["items"][0]
+
+    async def get_hash(self, state):
+        return state["id"]["videoId"]
+
+    async def get_pack(self, video):
+        embed = discord.Embed()
+        embed.title = video["snippet"]["title"]
+        embed.description = video["snippet"]["description"]
+
+        video_id = video["id"]["videoId"]
+        embed.url = f"https://youtube.com/watch?v={video_id}"
+
+        embed.set_image(url=video["snippet"]["thumbnails"]["high"]["url"])
+
+        return "", [], embed
+
+    async def channel_name(self, channel_id):
+        async with self.session.get(
+                "https://youtube.googleapis.com/youtube/v3/search",
+                params={
+                    "part": "snippet",
+                    "channelId": channel_id,
+                    "maxResults": "1",
+                    "type": "channel",
+                    "key": self.key
+                }) as response:
+            response = await response.json()
+
+        if response["pageInfo"]["totalResults"] < 1:
+            raise commands.CommandError("No results found!")
+
+        return response["items"][0]["snippet"]["title"]
 
     @commands.command()
     async def channel(self, ctx, *, search: str):
@@ -73,16 +108,59 @@ class Youtube(base.BaseCog):
     async def latestvid(self, ctx, *, search: str):
         "Display a YouTube channel's latest video"
 
-        video = await self.get_latest(await self.get_channel(search))
+        channel_id = (await self.get_channel(search))["id"]["channelId"]
+        video = await self.get_state(channel_id)
 
-        embed = discord.Embed()
-        embed.title = video["snippet"]["title"]
-        embed.description = video["snippet"]["description"]
+        await self.pack_send(ctx, *(await self.get_pack(video)))
 
-        video_id = video["id"]["videoId"]
-        embed.url = f"https://youtube.com/watch?v={video_id}"
+    @commands.command()
+    async def channelwatch(self, ctx, *, search: str):
+        "Get updates for a YouTube channel"
 
-        embed.set_image(url=video["snippet"]["thumbnails"]["high"]["url"])
+        if ctx.guild:
+            if not ctx.channel.permissions_for(ctx.author).administrator:
+                raise commands.CommandError(
+                    "To prevent spam, only administrators can watch channels.")
+
+        channel_id = (await self.get_channel(search))["id"]["channelId"]
+        await self.watch.register(ctx.channel, channel_id)
+
+    @commands.command()
+    async def channelunwatch(self, ctx, *, search: str):
+
+        if ctx.guild:
+            if not ctx.channel.permissions_for(ctx.author).administrator:
+                raise commands.CommandError(
+                    "To prevent spam, only administrators can watch channels.")
+
+        channel_id = (await self.get_channel(search))["id"]["channelId"]
+        await self.watch.unregister(ctx.channel, channel_id)
+
+    @commands.command()
+    async def channelwatching(self, ctx, *, search: str):
+        channel_id = (await self.get_channel(search))["id"]["channelId"]
+        name = await self.channel_name(channel_id)
+
+        if await self.watch.is_registered(ctx.channel, channel_id):
+            await ctx.send(
+                f"{ctx.channel.mention} is currently watching {name}.")
+        else:
+            await ctx.send(
+                f"{ctx.channel.mention} is not currently watching {name}.")
+
+    @commands.command()
+    async def channelslist(self, ctx):
+        "List the channels currently being watched"
+
+        ids = await self.watch.get_targets(ctx.channel)
+        watching = [await self.channel_name(id) for id in ids]
+
+        if ctx.guild:
+            name = f"#{ctx.channel.name}"
+        else:
+            name = f"@{ctx.author.display_name}"
+        embed = discord.Embed(title=f"{name} is watching...")
+        embed.description = ", ".join(name for name in watching)
 
         await ctx.send(embed=embed)
 
