9855036db2ecf84ca16f72dd2fbbf96124ed7be6
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-12-24 22:41:29 -0500

    Use cards.breq.dev for profile cards

--- extensions/profile/card.py
+++ extensions/profile/card.py
@@ -1,26 +1,12 @@
 import typing
 import io
-import textwrap
 
-from PIL import Image, ImageDraw, ImageFont, ImageEnhance, ImageOps
 import aiohttp
-from resizeimage import resizeimage
 
 import discord
 from discord.ext import commands
 
 from .. import base
-
-bigfont = ImageFont.truetype(
-    "fonts/UbuntuMono-R.ttf", 72, encoding="unic")
-
-smallfont = ImageFont.truetype(
-    "fonts/UbuntuMono-R.ttf", 36, encoding="unic")
-
-size = (256, 256)
-mask = Image.new('L', size, 0)
-draw = ImageDraw.Draw(mask)
-draw.ellipse((0, 0) + size, fill=255)
 
 
 class Card(base.BaseCog):
@@ -29,8 +15,13 @@ class Card(base.BaseCog):
     category = "Profile"
 
     fields = {
-        "desc": "Set your profile card description!",
-        "bg": "Set the URL for your background image!"
+        "bio": "Set your profile card description!",
+        # "background_image": "Set the URL for your background image!"
+    }
+
+    defaults = {
+        "bio": "",
+        "background_image": "https://breq.dev/assets/images/pansexual.png"
     }
 
     @commands.group(invoke_without_command=True)
@@ -40,50 +31,26 @@ class Card(base.BaseCog):
         if not user:
             user = ctx.author
 
-        # Add user background
-        url = (await self.redis.hget(f"profile:{ctx.guild.id}:{user.id}", "bg")
-               or "https://breq.dev/assets/images/logo/white_wireframe.jpg")
+        params = {
+            field:
+                (await self.redis.hget(
+                    f"profile:{ctx.guild.id}:{user.id}", field)
+                 or self.defaults[field])
+            for field in self.fields
+        }
+
+        params["name"] = user.display_name
+        # params["avatar"] = str(user.avatar_url)
+        params["background_image"] = str(user.avatar_url)
+        params["format"] = "png"
+        params["template"] = "background-image"
+
         async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                imagebytes = io.BytesIO(await response.read())
-                image = Image.open(imagebytes).convert("RGB")
+            async with session.get(
+                    "https://cards.breq.dev/card", params=params) as response:
+                file = io.BytesIO(await response.read())
 
-        image = resizeimage.resize_cover(image, (720, 376))
-
-        enhancer = ImageEnhance.Brightness(image)
-        image = enhancer.enhance(0.5)
-
-        draw = ImageDraw.Draw(image)
-
-        # Add user avatar
-        avatar = io.BytesIO(
-            await user.avatar_url_as(format="png", size=256).read())
-
-        avatar = Image.open(avatar)
-
-        avatar = ImageOps.fit(avatar, mask.size, centering=(0.5, 0.5))
-
-        image.paste(avatar, (32, 32), mask=mask)
-
-        # Add display name
-        wrap_name = "\n".join(textwrap.wrap(user.display_name, 9))
-        # text_height = (wrap_name.count("\n")+1) * 80
-        draw.multiline_text((320, 32), wrap_name,
-                            spacing=8, font=bigfont, fill=(255, 255, 255))
-
-        # Add user desc
-        desc = await self.redis.hget(
-            f"profile:{ctx.guild.id}:{user.id}", "desc") or ""
-        # wrap_desc = "\n".join(textwrap.wrap(desc, 20))
-        wrap_desc = desc[:30]
-        width, height = draw.textsize(wrap_desc, font=smallfont)
-        draw.multiline_text((360 - width/2, 320), wrap_desc,
-                            font=smallfont, fill=(255, 255, 255))
-
-        file = io.BytesIO()
-        image.save(file, "png")
-        file.seek(0)
-        file = discord.File(file, "profile.png")
+        file = discord.File(file, "card.png")
 
         await ctx.send(file=file)
 
